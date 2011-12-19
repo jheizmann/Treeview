a24c742b7a04e2a387d404f21f0cdc9b0c47cf52
schweitz <schweitz>
2011-12-19 13:28:25 +0000

    Finished migration to MW 1.17 and SMW 1.6

--- tests/LocalSettings.php
+++ tests/LocalSettings.php
@@ -18,19 +18,20 @@
  */
 
 #Import SMW, SMWHalo
-include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
+include_once('extensions/SemanticMediaWiki/SemanticMediaWiki.php');
 enableSemantics('http://wiki', true);
  
 include_once('extensions/SMWHalo/includes/SMW_Initialize.php');
-enableSMWHalo('SMWHaloStore2');
+enableSMWHalo();
+
+require_once('extensions/EnhancedRetrieval/includes/EnhancedRetrieval.php');
 
 #Semantic Treeview 
-require_once('extensions/Treeview/Treeview.php');
+require_once('extensions/Treeview/includes/TV_Initialize.php');
+enableTreeView();
 
-###Each extension wich depends on SMWHalo depends also on arclibrary, scriptmanager and deployment framework####
-require_once('deployment/Deployment.php');
-require_once("extensions/ScriptManager/SM_Initialize.php");
+###Each extension wich depends on SMWHalo depends also on arclibrary####
 include_once('extensions/ARCLibrary/ARCLibrary.php');
-enableARCLibrary();
+
 ################################################################################################################
 
